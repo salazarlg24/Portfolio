96043770c9e0f835186fc9f65c5e93c7417084b4
Kua <salazarlg24@gmail.com>
2017-06-09 15:03:34 -0400

    second commit

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var mongoose = require( 'mongoose' ),
 app.use( express.static( path.join( root, 'client' )));
 app.use( express.static( path.join( root, 'bower_components' )));
 app.use(bp.json())
-require('./server/config/mongoose.js');
-require('./server/config/routes.js')(app);
 app.listen( port, function() {
   console.log( `server running on port ${ port }` );
 });
